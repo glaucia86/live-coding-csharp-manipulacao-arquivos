35b9baaaae7c70795d0a6decf4df5192c96c2cdf
glaucia86 <glaucia_lemos86@hotmail.com>
2019-02-04 22:19:14 -0200

    Início do desenvolvimento do projeto de manipulação de arquivos

---
+++ Live-Coding/ManipulacaoArquivos/Input/LeituraProduto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManipulacaoArquivos.Input
+{
+    public class LeituraProduto
+    {
+        #region Método LerCodigoProduto
+
+        public int LerCodigoProduto()
+        {
+            Console.Write("Informe o Código do Produto...: ");
+            int codigo = Convert.ToInt32(Console.ReadLine()); 
+
+            return codigo;
+        }
+        #endregion
+
+        #region Método LerNomeProduto
+        public string LerNomeProduto()
+        {
+            Console.Write("Informe o Nome do Produto...: ");
+            string nome = Console.ReadLine();
+
+            return nome;
+        }
+        #endregion
+
+        #region Método LerPrecoProduto
+        public string LerPrecoProduto()
+        {
+            Console.Write("Informe o Preço do Produto...: ");
+            decimal preco = Convert.ToDecimal(Console.ReadLine());
+
+            return preco;
+        }
+        #endregion
+    }
+}
---
+++ Live-Coding/ManipulacaoArquivos/Model/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace ManipulacaoArquivos.Model
+{
+    public class Produto
+    {
+        public int CodigoProduto { get; set; }
+
+        public string NomeProduto { get; set; }
+
+        public decimal PrecoProduto { get; set; }
+    }
+}
